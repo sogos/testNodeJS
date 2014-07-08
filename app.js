ed7cc1f6824f07496b603cd7d3fd80fa62389945
Thibault <thibault.cordier@gmail.com>
2014-07-08 12:01:31 +0200

    Change Default Port

--- app.js
+++ app.js
@@ -3,6 +3,6 @@ var app = express();
 app.get('/', function(req, res){
   res.send('Hello World');
 });
-var server = app.listen(80, function() {
+var server = app.listen(8080, function() {
     console.log('Listening on port %d', server.address().port);
 });
